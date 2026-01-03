27d035e7d10e950fca5ce21961711c833fbd3ac8
TuanAnh <bnshuyenthoainvta@gmail.com>
2026-01-03 21:32:10 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const paypal = require('./services/paypal');
+const paypal = require("./services/paypal");
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index");
 });
+
+
+app.post("/pay", async (req, res) => {
+    try {
+        const url = await paypal.createOrder();
+        res.redirect(url);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get("/complete-order", async (req, res) => {
+    try {
+        await paypal.capturePayment(req.query.token);
+
+        res.send("Course purchased successfully");
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get("/cancel-order", (req, res) => {
+    res.redirect("/");
+});
+
 
 app.listen(3000, () => console.log("Server running on port 3000"));
